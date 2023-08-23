2f53265fa8cf372d7a67a1c496910c552fd9a372
MarufUsmonovdev <marufusmonov1996@gmail.com>
2023-08-23 18:16:40 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Adding   {a + b}");
+System.Console.WriteLine($"Sub      {a - b}");
+System.Console.WriteLine($"Div      {a / b}");
+System.Console.WriteLine($"Multiply {a * b}");
+
+
+System.Console.WriteLine($"Remainder{a % b}");
