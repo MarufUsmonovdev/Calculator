34ad5603886a0f916418c83ca4f163388e31a5cb
MarufUsmonovdev <142956091+MarufUsmonovdev@users.noreply.github.com>
2023-08-25 06:35:52 +0500

    if Condition

--- Program.cs
+++ Program.cs
@@ -5,9 +5,24 @@ int firstnumber = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Enter secondnumber: ");
 int secondnumber = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine($"1st number is greater then 2dn number: {firstnumber > secondnumber}");
-System.Console.WriteLine($"1st number is les then 2dn number: {firstnumber < secondnumber}");
-System.Console.WriteLine($"1st number is the same with 2dn number: {firstnumber == secondnumber}");
-System.Console.WriteLine($"1st number is greater or same then 2dn number: {firstnumber >= secondnumber}");
-System.Console.WriteLine($"1st number is les or same 2dn number: {firstnumber <= secondnumber}");
-System.Console.WriteLine($"1st number is not equel 2dn number: {firstnumber != secondnumber}");
+if(firstnumber > secondnumber)
+{
+    System.Console.WriteLine("First number is bigger.");
+}
+else if(firstnumber == secondnumber)
+{
+    System.Console.WriteLine("Both number is equel.");
+}
+else
+{
+    System.Console.WriteLine("Second number is bigger.");
+}
+
+
+
+// System.Console.WriteLine($"1st number is greater then 2dn number: {firstnumber > secondnumber}");
+// System.Console.WriteLine($"1st number is les then 2dn number: {firstnumber < secondnumber}");
+// System.Console.WriteLine($"1st number is the same with 2dn number: {firstnumber == secondnumber}");
+// System.Console.WriteLine($"1st number is greater or same then 2dn number: {firstnumber >= secondnumber}");
+// System.Console.WriteLine($"1st number is les or same 2dn number: {firstnumber <= secondnumber}");
+// System.Console.WriteLine($"1st number is not equel 2dn number: {firstnumber != secondnumber}");
